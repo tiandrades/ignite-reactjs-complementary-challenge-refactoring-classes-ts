bc3290cc12380117129037afa5ae1bfca9da357f
Tiago Andrade <tailergamer7@gmail.com>
2021-03-21 12:03:03 -0400

    :sparkles: Challenge finalized!

--- src/components/Food/styles.ts
+++ src/components/Food/styles.ts
@@ -1,6 +1,10 @@
 import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface FoodComponentProps {
+  available: boolean;
+}
+
+export const Container = styled.div<FoodComponentProps>`
   background: #f0f0f5;
   border-radius: 8px;
 
